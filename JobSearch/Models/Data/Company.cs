b59d52829c27bccd1cb59562a10bc108b9ef602a
Jerry Bloom <mrjerrybloom@yahoo.com>
2017-07-19 08:58:40 -0400

    Set up DB connection

---
+++ JobSearch/Models/Data/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace JobSearch.Models.Data
+{
+    [Table("tblCompany")]
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyWebsite { get; set; }
+        [Column("CompanyType_Id")]
+        public int CompanyTypeId { get; set; }
+        public string CompanyUserName { get; set; }
+        public string CompanyUserPassword { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool IsActive { get; set; }
+
+        [ForeignKey("CompanyTypeId")]
+        public virtual CompanyType CompanyType { get; set; }
+    }
+}
---
+++ JobSearch/Models/Data/JobSearchDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JobSearch.Models.Data
+{
+    public class JobSearchDbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<CompanyType> CompanyTypes { get; set; }
+    }
+}
